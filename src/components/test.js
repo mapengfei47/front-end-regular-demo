9aadc1685e302525cbe9b62460f7c0db607e4661
allanma <allanma@xiaoe-tech.com>
2024-09-29 18:26:49 +0800

    feat: 去掉注释

--- src/components/test.js
+++ src/components/test.js
@@ -2,9 +2,10 @@
  * @Author: allanma allanma@xiaoe-tech.com
  * @Date: 2024-09-27 17:31:17
  * @LastEditors: allanma allanma@xiaoe-tech.com
- * @LastEditTime: 2024-09-29 14:44:03
+ * @LastEditTime: 2024-09-29 17:29:03
  * @FilePath: /front-end-regular-demo/src/components/test.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 function test() {}
 test()
+test('111')
